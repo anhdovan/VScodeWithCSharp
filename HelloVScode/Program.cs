8df7fd1e06f4a54f3c6f291fb8fd32c5bf3abac3
anhdovan <dvanh1974@yahoo.com>
2023-08-28 17:48:31 +0700

    delete some files

--- HelloVScode/Program.cs
+++ HelloVScode/Program.cs
@@ -2,8 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World! Please enter your name");
         var s = Console.ReadLine();
         Console.WriteLine($"You entered: {s}");
+        DoSomething();
+    }
+    private static void DoSomething()
+    {
+        Console.WriteLine("I am doing something");
     }
 }
