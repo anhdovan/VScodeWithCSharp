34da104e157f8c09615d578c399a71366e57f2f5
anhdovan <dvanh1974@yahoo.com>
2023-08-28 16:38:27 +0700

    Initial

---
+++ HelloVScode/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var s = Console.ReadLine();
+        Console.WriteLine($"You entered: {s}");
+    }
+}
